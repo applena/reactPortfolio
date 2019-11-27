8ce88b4d8f3242d7fd8b475bfdf824a7dea1ae4c
LENA EIVY <applena@gmail.com>
2019-11-26 16:52:52 -0800

    added react know your vote

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -3,7 +3,7 @@ import './portfolio.scss';
 import Bazaar from '../../img/bazaar.png';
 import ToDoPic from '../../img/pic02.png';
 import Photography from '../../img/pic04.png';
-import Dna from '../../img/pic06.png';
+import VoteReact from '../../img/knowVote.png';
 import Vote from '../../img/pic07.png';
 import Waldo from '../../img/pic9.png';
 import './hoverLinks.css';
@@ -35,8 +35,8 @@ class Portfolio extends React.Component{
             <h3>WALDO</h3>
           </div>
           <div id="mom" class="card">
-            <a href="http://dnadialogues.com/" target="blank"><img src={Dna} /></a>
-            <h3>DNA Dialogues</h3>
+            <a href="http://lenaeivy.com/know-your-vote" target="blank"><img src={VoteReact} /></a>
+            <h3>Know Your Vote (redone with React Frontend)</h3>
           </div>
           <div id="todo" class="card">
             <a href="https://applena.github.io/todolist/" target="blank"><img src={ToDoPic} /></a>
